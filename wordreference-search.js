c673fe2d4a510f474a6ab689648be7084dec2d37
andrtw <andrea96.t@gmail.com>
2024-12-21 16:13:15 +0100

    Fix formatting

--- wordreference-search.js
+++ wordreference-search.js
@@ -15,20 +15,20 @@
 // @grant        none
 // ==/UserScript==
 
-const BASE_URL = 'https://www.wordreference.com';
+const BASE_URL = "https://www.wordreference.com";
 const CODE_ESC = 27;
-const LANG = 'enit'; // English to Italian
+const LANG = "enit"; // English to Italian
 
 function log(message, data) {
-    if (data) {
-        console.log(message, data);
-    } else {
-        console.log(message);
-    }
+  if (data) {
+    console.log(message, data);
+  } else {
+    console.log(message);
+  }
 }
 
 function injectStyle(headElem) {
-    const css = `
+  const css = `
 .wr-box {
   display: none;
   background: #fff;
@@ -111,208 +111,219 @@ function injectStyle(headElem) {
   }
 }
 `;
-    const style = document.createElement('style');
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    headElem.appendChild(style);
+  const style = document.createElement("style");
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  headElem.appendChild(style);
 }
 
 function getSelectedText() {
-    let selection = '';
-    if (window.getSelection) {
-        selection = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != 'Control') {
-        selection = document.selection.createRange().text;
-    }
-    const pieces = selection.split(' ').filter((p) => p.length > 0);
-    if (pieces.length == 1) {
-        return pieces[0].replace(/\r?\n|\r/g, '').trim();
-    }
-    return undefined;
+  let selection = "";
+  if (window.getSelection) {
+    selection = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    selection = document.selection.createRange().text;
+  }
+  const pieces = selection.split(" ").filter((p) => p.length > 0);
+  if (pieces.length == 1) {
+    return pieces[0].replace(/\r?\n|\r/g, "").trim();
+  }
+  return undefined;
 }
 
 function clearSelection() {
-    if (window.getSelection) {
-        if (window.getSelection().empty) {
-            window.getSelection().empty();
-        } else if (window.getSelection().removeAllRanges) {
-            window.getSelection().removeAllRanges();
-        }
-    } else if (document.selection) {
-        document.selection.empty();
+  if (window.getSelection) {
+    if (window.getSelection().empty) {
+      window.getSelection().empty();
+    } else if (window.getSelection().removeAllRanges) {
+      window.getSelection().removeAllRanges();
     }
+  } else if (document.selection) {
+    document.selection.empty();
+  }
 }
 
 class BoxController {
-    #isOpen = false;
+  #isOpen = false;
 
-    constructor() {
-        this.boxElem = this.#buildBoxElem();
-        this.loadingElem = this.#buildLoadingElem();
-        this.contentElem = this.#buildContentElem();
-    }
+  constructor() {
+    this.boxElem = this.#buildBoxElem();
+    this.loadingElem = this.#buildLoadingElem();
+    this.contentElem = this.#buildContentElem();
+  }
 
-    get isOpen() {
-        return this.#isOpen;
-    }
+  get isOpen() {
+    return this.#isOpen;
+  }
 
-    get box() {
-        this.boxElem.appendChild(this.loadingElem);
-        this.boxElem.appendChild(this.contentElem);
-        return this.boxElem;
-    }
+  get box() {
+    this.boxElem.appendChild(this.loadingElem);
+    this.boxElem.appendChild(this.contentElem);
+    return this.boxElem;
+  }
 
-    searchTerm(term, x, y) {
-        if (!term) return;
+  searchTerm(term, x, y) {
+    if (!term) return;
 
-        this.#open(x, y);
+    this.#open(x, y);
 
-        fetch(`${BASE_URL}/${LANG}/${term}`)
-            .then((response) => response.text())
-            .then((html) => {
-                const dom = new DOMParser().parseFromString(html, 'text/html');
-                const noEntryFoundElem = dom.querySelector('#noEntryFound');
+    fetch(`${BASE_URL}/${LANG}/${term}`)
+      .then((response) => response.text())
+      .then((html) => {
+        const dom = new DOMParser().parseFromString(html, "text/html");
+        const noEntryFoundElem = dom.querySelector("#noEntryFound");
 
-                if (noEntryFoundElem) {
-                    this.close();
-                    return;
-                }
-
-                const headerWordElem = dom.querySelector('.headerWord');
-                const word = headerWordElem.childElementCount > 0 ? headerWordElem.childNodes[0].innerHTML : headerWordElem.innerHTML;
-                const pron = this.#getPronunciation(dom.querySelector('#pronunciation_widget'));
-                const audioUrl = this.#getAudioUrl(dom.querySelector('#listen_widget script')?.text);
-                this.#populateBox(word, pron, audioUrl);
-            })
-            .catch((error) => log('Error calling WordReference', error));
-    }
-
-    close() {
-        if (!this.#isOpen) return;
-
-        this.boxElem.style.display = 'none';
-        this.#isOpen = false;
-    }
-
-    #buildBoxElem() {
-        const boxElem = document.createElement('div');
-        boxElem.classList.add('wr-box');
-
-        const stopPropagation = (e) => e.stopPropagation();
-        boxElem.onclick = stopPropagation;
-        boxElem.onmouseup = stopPropagation;
-        boxElem.onmousedown = stopPropagation;
-
-        return boxElem;
-    }
-
-    #buildContentElem() {
-        const boxContentElem = document.createElement('div');
-        boxContentElem.classList.add('wr-box-content');
-        return boxContentElem;
-    }
-
-    #buildLoadingElem() {
-        const loadingElem = document.createElement('span');
-        loadingElem.classList.add('wr-loading');
-        return loadingElem;
-    }
-
-    #open(x, y) {
-        if (this.#isOpen) return;
-
-        this.loadingElem.style.display = 'block';
-        this.contentElem.style.display = 'none';
-
-        this.boxElem.style.top = `${y || 0}px`;
-        this.boxElem.style.left = `${x || 0}px`;
-        this.boxElem.style.display = 'block';
-
-        this.#isOpen = true;
-    }
-
-    #populateBox(term, pron, audioUrl) {
-        this.loadingElem.style.display = 'none';
-        this.contentElem.style.display = 'flex';
-
-        const logoElem = document.createElement('div');
-        logoElem.classList.add('wr-logo');
-        logoElem.onclick = (e) => { window.open(`${BASE_URL}/${LANG}/${term}`); };
-
-        const rightColElem = document.createElement('div');
-        rightColElem.classList.add('wr-box-content-right');
-
-        if (!pron && !audioUrl) {
-            rightColElem.style.display = 'none';
-        }
-        if (pron) {
-            const p = document.createElement('span');
-            p.classList.add('wr-pron');
-            p.innerHTML = pron;
-            rightColElem.appendChild(p);
-        }
-        if (audioUrl) {
-            const icon = document.createElement('div');
-            icon.classList.add('wr-audio-icon');
-            icon.onclick = (e) => {
-                const audio = new Audio(BASE_URL + audioUrl);
-                audio.play();
-            };
-            rightColElem.appendChild(icon);
+        if (noEntryFoundElem) {
+          this.close();
+          return;
         }
 
-        this.contentElem.innerHTML = '';
-        this.contentElem.appendChild(logoElem);
-        this.contentElem.appendChild(rightColElem);
+        const headerWordElem = dom.querySelector(".headerWord");
+        const word =
+          headerWordElem.childElementCount > 0
+            ? headerWordElem.childNodes[0].innerHTML
+            : headerWordElem.innerHTML;
+        const pron = this.#getPronunciation(
+          dom.querySelector("#pronunciation_widget"),
+        );
+        const audioUrl = this.#getAudioUrl(
+          dom.querySelector("#listen_widget script")?.text,
+        );
+        this.#populateBox(word, pron, audioUrl);
+      })
+      .catch((error) => log("Error calling WordReference", error));
+  }
+
+  close() {
+    if (!this.#isOpen) return;
+
+    this.boxElem.style.display = "none";
+    this.#isOpen = false;
+  }
+
+  #buildBoxElem() {
+    const boxElem = document.createElement("div");
+    boxElem.classList.add("wr-box");
+
+    const stopPropagation = (e) => e.stopPropagation();
+    boxElem.onclick = stopPropagation;
+    boxElem.onmouseup = stopPropagation;
+    boxElem.onmousedown = stopPropagation;
+
+    return boxElem;
+  }
+
+  #buildContentElem() {
+    const boxContentElem = document.createElement("div");
+    boxContentElem.classList.add("wr-box-content");
+    return boxContentElem;
+  }
+
+  #buildLoadingElem() {
+    const loadingElem = document.createElement("span");
+    loadingElem.classList.add("wr-loading");
+    return loadingElem;
+  }
+
+  #open(x, y) {
+    if (this.#isOpen) return;
+
+    this.loadingElem.style.display = "block";
+    this.contentElem.style.display = "none";
+
+    this.boxElem.style.top = `${y || 0}px`;
+    this.boxElem.style.left = `${x || 0}px`;
+    this.boxElem.style.display = "block";
+
+    this.#isOpen = true;
+  }
+
+  #populateBox(term, pron, audioUrl) {
+    this.loadingElem.style.display = "none";
+    this.contentElem.style.display = "flex";
+
+    const logoElem = document.createElement("div");
+    logoElem.classList.add("wr-logo");
+    logoElem.onclick = (e) => {
+      window.open(`${BASE_URL}/${LANG}/${term}`);
+    };
+
+    const rightColElem = document.createElement("div");
+    rightColElem.classList.add("wr-box-content-right");
+
+    if (!pron && !audioUrl) {
+      rightColElem.style.display = "none";
+    }
+    if (pron) {
+      const p = document.createElement("span");
+      p.classList.add("wr-pron");
+      p.innerHTML = pron;
+      rightColElem.appendChild(p);
+    }
+    if (audioUrl) {
+      const icon = document.createElement("div");
+      icon.classList.add("wr-audio-icon");
+      icon.onclick = (e) => {
+        const audio = new Audio(BASE_URL + audioUrl);
+        audio.play();
+      };
+      rightColElem.appendChild(icon);
     }
 
-    #getPronunciation(pronElem) {
-        if (!pronElem) return undefined;
+    this.contentElem.innerHTML = "";
+    this.contentElem.appendChild(logoElem);
+    this.contentElem.appendChild(rightColElem);
+  }
 
-        const deleteElems = pronElem.querySelectorAll('span[style="font-size:12px"]');
-        deleteElems.forEach((el) => {
-            el.parentNode.removeChild(el);
-        });
-        return pronElem.innerText
-            .replace(/UK/g, '\nUK')
-            .replace(/US/g, '\nUS')
-            .trim();
-    }
+  #getPronunciation(pronElem) {
+    if (!pronElem) return undefined;
 
-    #getAudioUrl(scriptString) {
-        if (!scriptString) return undefined;
-        const urls = scriptString.match(/('\/).*(mp3')/)[0]?.split(',');
-        return urls[0]?.replace(/'/g, '');
-    }
+    const deleteElems = pronElem.querySelectorAll(
+      'span[style="font-size:12px"]',
+    );
+    deleteElems.forEach((el) => {
+      el.parentNode.removeChild(el);
+    });
+    return pronElem.innerText
+      .replace(/UK/g, "\nUK")
+      .replace(/US/g, "\nUS")
+      .trim();
+  }
+
+  #getAudioUrl(scriptString) {
+    if (!scriptString) return undefined;
+    const urls = scriptString.match(/('\/).*(mp3')/)[0]?.split(",");
+    return urls[0]?.replace(/'/g, "");
+  }
 }
 
-(function() {
-    const head = document.querySelector('head');
-    injectStyle(head);
+(function () {
+  const head = document.querySelector("head");
+  injectStyle(head);
 
-    const boxController = new BoxController();
-    document.body.appendChild(boxController.box);
+  const boxController = new BoxController();
+  document.body.appendChild(boxController.box);
 
-    document.onclick = (e) => {
-        if (boxController.isOpen) {
-            boxController.close();
-        } else {
-            boxController.searchTerm(getSelectedText(), e.pageX + 2, e.pageY + 2);
-        }
-    };
+  document.onclick = (e) => {
+    if (boxController.isOpen) {
+      boxController.close();
+    } else {
+      boxController.searchTerm(getSelectedText(), e.pageX + 2, e.pageY + 2);
+    }
+  };
 
-    document.onmousedown = (e) => {
-        boxController.close();
-        clearSelection();
-    };
+  document.onmousedown = (e) => {
+    boxController.close();
+    clearSelection();
+  };
 
-    document.onkeydown = (e) => {
-        if (boxController.isOpen && e.keyCode === CODE_ESC) {
-            boxController.close();
-            clearSelection();
-        }
-    };
+  document.onkeydown = (e) => {
+    if (boxController.isOpen && e.keyCode === CODE_ESC) {
+      boxController.close();
+      clearSelection();
+    }
+  };
 })();
