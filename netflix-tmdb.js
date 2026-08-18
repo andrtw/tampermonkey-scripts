b3b6376380b4928ebc45fbcb5fbf3503d7af36ca
andrtw <andrea96.t@gmail.com>
2026-08-18 20:30:18 +0200

    Netflix-tmdb: search current movie

--- netflix-tmdb.js
+++ netflix-tmdb.js
@@ -8,6 +8,21 @@
 // @grant        none
 // ==/UserScript==
 
+const TMDB_READ_ACCESS_TOKEN = "YOU_ACCESS_TOKEN";
+
+const ENTITY_MOVIE = "movie";
+const ENTITY_SERIES = "series";
+
+const TMDB_API_BASE_URL = "https://api.themoviedb.org";
+const TMDB_API_VERSION = "3";
+const TMDB_API_MOVIE_PATH = "movie";
+const TMDB_API_SERIES_PATH = "tv";
+const TMDB_API_PATHS = {
+  [ENTITY_MOVIE]: TMDB_API_MOVIE_PATH,
+  [ENTITY_SERIES]: TMDB_API_SERIES_PATH,
+};
+
+// #region DOM
 function injectStyle(headElem) {
   const css = `
 `;
@@ -20,8 +35,105 @@ function injectStyle(headElem) {
   headElem.appendChild(style);
 }
 
+function waitForElement(selector, predicate) {
+  function ensurePredicate(elem) {
+    if (!predicate) return true;
+    return predicate(elem);
+  }
+
+  return new Promise((resolve) => {
+    const elem = document.querySelector(selector);
+    if (elem && ensurePredicate(elem)) {
+      return resolve(elem);
+    }
+
+    const observer = new MutationObserver(() => {
+      const elem = document.querySelector(selector);
+      if (elem && ensurePredicate(elem)) {
+        observer.disconnect();
+        resolve(elem);
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+// #endregion
+
+// #region TMDB
+/**
+ * Helper method for sending a request to the TMDB API.
+ * By default, it adds common headers like content type
+ * and authorization token.
+ */
+function tmdbRequest(url, config) {
+  const c = {
+    ...config,
+    headers: {
+      ...(config?.headers ?? {}),
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TMDB_READ_ACCESS_TOKEN}`,
+    },
+  };
+  return fetch(url, c);
+}
+
+function tmdbUrl(path) {
+  return `${TMDB_API_BASE_URL}/${TMDB_API_VERSION}/${path}`;
+}
+// #endregion
+
+// #region Netflix
+/**
+ * Returns the type of the current entity: can be either "movie"
+ * or "series".
+ *
+ * The value is used to different extents to differentiate between
+ * the two types in the TMDB API.
+ */
+async function getEntityType() {
+  const durationElem = await waitForElement(
+    ".videoMetadata--container .duration",
+  );
+  const duration = durationElem.textContent;
+  const isMovie = /^(\dh)?\s?(\d{1,2}m)?$$/.test(duration);
+  if (isMovie) {
+    return ENTITY_MOVIE;
+  } else {
+    return ENTITY_SERIES;
+  }
+}
+
+async function getTitle() {
+  const elem = await waitForElement(
+    ".about-header strong",
+    (elem) => elem?.textContent,
+  );
+  return elem.textContent;
+}
+// #endregion
+
+async function search(entity, title) {
+  const path = TMDB_API_PATHS[entity];
+  const url = new URL(tmdbUrl(`search/${path}`));
+  url.searchParams.append("query", title);
+  const response = await tmdbRequest(url);
+  const body = await response.json();
+  return body?.results?.[0];
+}
+
 async function onDetailsOpened() {
-  console.log("onDetailsOpened");
+  const entity = await getEntityType();
+  const title = await getTitle();
+  const result = await search(entity, title);
+  if (result) {
+    console.log(result.overview, result.vote_average, result.vote_count);
+  } else {
+    console.log("No results");
+  }
 }
 
 const URLS_HANDLER = {
